9019713fd084f1ce3f576a429b53d4c880a26eb7
Riley Labrecque <git@rileylabrecque.com>
2020-11-27 14:16:40 -0800

    Add services/run.sh

---
+++ services/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Bash Strict Mode
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Pull down the any updated images
+docker-compose pull
+
+# Update and restart the services
+docker-compose up -d --build
