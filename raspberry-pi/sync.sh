8237bb6009cedf54690ac8994e6b382a92a63b81
Riley Labrecque <git@rileylabrecque.com>
2021-06-07 12:01:25 -0700

    raspberry-pi: Update sync.sh script

--- raspberry-pi/sync.sh
+++ raspberry-pi/sync.sh
@@ -7,4 +7,10 @@ IFS=$'\n\t'
 
 # Copies the services folder over to the raspberry pi.
 
+echo Syncing install.sh
+rsync -r ./install.sh pi@raspberrypi.local:~/install.sh
+
+echo Syncing services/
 rsync -r ./services/ pi@raspberrypi.local:~/services
+
+echo Complete
