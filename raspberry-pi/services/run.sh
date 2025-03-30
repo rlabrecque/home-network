0ca9afeb30a38aaa1f295997a68d196414364a77
Riley Labrecque <git@rileylabrecque.com>
2025-03-30 14:00:14 -0700

    services: run.sh: Prune old images

--- raspberry-pi/services/run.sh
+++ raspberry-pi/services/run.sh
@@ -5,8 +5,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Pull down the any updated images
+# Pull down the any updated images, we explicity do this first because we need to download the images
+# before we can restart the services.
 docker compose pull
 
 # Update and restart the services
 docker compose up -d --build --remove-orphans
+
+# Prune any old dnaling images
+docker image prune -a
